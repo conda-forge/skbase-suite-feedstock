6aaa16383c813071e4a8f3366c64f1a7ca2f8dac
Franz Király <f.kiraly@ucl.ac.uk>
2024-05-22 21:18:12 +0100

    Update build_script.sh

--- recipe/build_script.sh
+++ recipe/build_script.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-set -e
-set -x  # Enable script debugging for verbose logging
-
-# Specify the Python executable path for arm64 architecture
-PYTHON_EXE="/opt/miniforge3/bin/python"
-
-# Install the package
-$PYTHON_EXE -m pip install . --no-deps -vv --no-build-isolation
+$PYTHON -m pip install . --no-deps -vv --no-build-isolation
